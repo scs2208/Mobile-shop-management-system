49c11db5df3ce2a0411699425942f7aef64934da
kmnsrimal <43135950+kmnsrimal@users.noreply.github.com>
2019-07-03 12:17:44 +0530

    Update phone.sql

--- phone.sql
+++ phone.sql
@@ -126,6 +126,59 @@ INSERT INTO `trans` (`Bill_No`, `Date`, `Customer_Name`, `Customer_Contact`, `It
 (1, '2019-06-12', 'R.N. Herath', 754426475, '1 SD card', '900.00', '002'),
 (2, '2019-06-14', 'D.K. Silva', 775538133, '1 Bluetooth Headset', '1500.00', '009'),
 (3, '2019-06-18', 'A.K. Perera', 712398455, '1 Samsung Galaxy J7', '31000.00', '001');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `Address` varchar(100) NOT NULL,
+  `tp_no` varchar(11) NOT NULL,
+  `attendence` int(11) NOT NULL,
+  `salary` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `registration`
+--
+
+INSERT INTO `registration` (`id`, `name`, `email`, `Address`, `tp_no`, `attendence`, `salary`, `username`, `password`) VALUES
+(28, 'dilan', 'srimal', 'no 150,colombo', '0772324243', 95, 50000, 'admin', 'ucsc@123'),
+(29, 'KMN', 'nandana', 'no 5,colombo 07', '0772324243', 80, 30000, 'adminucsc@123', 'aaa'),
+(30, 'dileep', 'nsrimal@gmail.com', 'colombo 8,', '0772324243', 90, 40000, 'admin', 'bb'),
+(31, 'ucsc', 'nsrimal@gmail.com', 'no 25,colombo 9', '0777847899', 90, 40000, 'dileep', '22'),
+(32, 'nandana', 'srimal@gmail.com', 'no 3,colombo 10', '0777847899', 95, 45000, 'admin', '3333'),
+(33, 'jekwekjew', 'nsrimal@gmail.com', 'no 50,colombo', '0777847899', 75, 45000, 'admin', 'ucsc@123'),
+(34, 'dilan', 'dilan@gmail.com', 'AWsded', '0772324243', 89, 45000, 'testuser', '3245464'),
+(35, 'sugath', 'dilan@gmail.com', 'no 7,colombo 7', '0772324243', 89, 45000, 'testuser', '3245464');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 COMMIT;
 
 
