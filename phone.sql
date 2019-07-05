13deb3e77be142fd35ad3f90461ab5821ecdf149
akilasithara69 <50179080+akilasithara69@users.noreply.github.com>
2019-07-05 10:17:50 +0530

    Add files via upload

--- phone.sql
+++ phone.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jul 01, 2019 at 06:44 PM
--- Server version: 5.7.19
--- PHP Version: 7.1.9
+-- Host: 127.0.0.1
+-- Generation Time: Jul 05, 2019 at 06:45 AM
+-- Server version: 10.1.40-MariaDB
+-- PHP Version: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,16 +28,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `persons`
 --
 
-DROP TABLE IF EXISTS `persons`;
-CREATE TABLE IF NOT EXISTS `persons` (
+CREATE TABLE `persons` (
   `fullName` varchar(40) NOT NULL,
   `nid` varchar(10) NOT NULL,
   `phoneNo` varchar(10) NOT NULL,
   `address` varchar(40) NOT NULL,
   `uName` varchar(10) NOT NULL,
   `uId` varchar(10) NOT NULL,
-  `password` varchar(10) NOT NULL,
-  PRIMARY KEY (`nid`)
+  `password` varchar(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -54,15 +52,13 @@ INSERT INTO `persons` (`fullName`, `nid`, `phoneNo`, `address`, `uName`, `uId`, 
 -- Table structure for table `stable`
 --
 
-DROP TABLE IF EXISTS `stable`;
-CREATE TABLE IF NOT EXISTS `stable` (
+CREATE TABLE `stable` (
   `Id` int(4) NOT NULL,
   `staffname` varchar(20) NOT NULL,
   `month` varchar(10) NOT NULL,
   `s_perday` int(5) NOT NULL,
   `attendance` int(3) NOT NULL,
-  `salary` int(50) NOT NULL,
-  PRIMARY KEY (`Id`)
+  `salary` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -78,27 +74,30 @@ INSERT INTO `stable` (`Id`, `staffname`, `month`, `s_perday`, `attendance`, `sal
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `telephone`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `name` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `userid` varchar(10) NOT NULL,
-  `level` int(11) NOT NULL,
-  PRIMARY KEY (`userid`),
-  UNIQUE KEY `userid` (`userid`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE `telephone` (
+  `name` varchar(100) NOT NULL,
+  `id` varchar(100) NOT NULL,
+  `The_brand` varchar(45) NOT NULL,
+  `Price_of_the_brand` int(10) NOT NULL,
+  `stock` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `telephone`
 --
 
-INSERT INTO `users` (`name`, `password`, `userid`, `level`) VALUES
-('kavishka', 'Hello@world', 'A000', 1),
-('janaka', 'janaka', 's001', 0),
-('kamal', 'kamal123', 's002', 0);
+INSERT INTO `telephone` (`name`, `id`, `The_brand`, `Price_of_the_brand`, `stock`) VALUES
+('htc ', '2356', 'Desire 728', 35000, 25),
+('htc', '2425', 'xyzd', 35000, 198),
+('nokia', '3454578', 'nokia asha', 19000, 49),
+('Htc', '356277', 'htc mia', 35000, 24),
+('samsung', '35652', 'galaxy', 12000, 200),
+('oppo', '37684', 'x2', 40000, 12),
+('oppo', '53656', 'oppx', 35000, 99),
+('nokia', '9898', 'x1', 26873, 25);
 
 -- --------------------------------------------------------
 
@@ -106,16 +105,14 @@ INSERT INTO `users` (`name`, `password`, `userid`, `level`) VALUES
 -- Table structure for table `trans`
 --
 
-DROP TABLE IF EXISTS `trans`;
-CREATE TABLE IF NOT EXISTS `trans` (
+CREATE TABLE `trans` (
   `Bill_No` int(4) NOT NULL,
   `Date` date NOT NULL,
   `Customer_Name` varchar(20) NOT NULL,
   `Customer_Contact` bigint(10) NOT NULL,
   `Item_Quantity` varchar(225) NOT NULL,
   `Price` varchar(20) NOT NULL,
-  `Cashier_ID` varchar(4) NOT NULL,
-  PRIMARY KEY (`Bill_No`)
+  `Cashier_ID` varchar(4) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -127,60 +124,63 @@ INSERT INTO `trans` (`Bill_No`, `Date`, `Customer_Name`, `Customer_Contact`, `It
 (2, '2019-06-14', 'D.K. Silva', 775538133, '1 Bluetooth Headset', '1500.00', '009'),
 (3, '2019-06-18', 'A.K. Perera', 712398455, '1 Samsung Galaxy J7', '31000.00', '001');
 
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `registration`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `registration` (
-  `id` int(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `Address` varchar(100) NOT NULL,
-  `tp_no` varchar(11) NOT NULL,
-  `attendence` int(11) NOT NULL,
-  `salary` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `users` (
+  `name` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `userid` varchar(10) NOT NULL,
+  `level` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `registration`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `registration` (`id`, `name`, `email`, `Address`, `tp_no`, `attendence`, `salary`, `username`, `password`) VALUES
-(28, 'dilan', 'srimal', 'no 150,colombo', '0772324243', 95, 50000, 'admin', 'ucsc@123'),
-(29, 'KMN', 'nandana', 'no 5,colombo 07', '0772324243', 80, 30000, 'adminucsc@123', 'aaa'),
-(30, 'dileep', 'nsrimal@gmail.com', 'colombo 8,', '0772324243', 90, 40000, 'admin', 'bb'),
-(31, 'ucsc', 'nsrimal@gmail.com', 'no 25,colombo 9', '0777847899', 90, 40000, 'dileep', '22'),
-(32, 'nandana', 'srimal@gmail.com', 'no 3,colombo 10', '0777847899', 95, 45000, 'admin', '3333'),
-(33, 'jekwekjew', 'nsrimal@gmail.com', 'no 50,colombo', '0777847899', 75, 45000, 'admin', 'ucsc@123'),
-(34, 'dilan', 'dilan@gmail.com', 'AWsded', '0772324243', 89, 45000, 'testuser', '3245464'),
-(35, 'sugath', 'dilan@gmail.com', 'no 7,colombo 7', '0772324243', 89, 45000, 'testuser', '3245464');
+INSERT INTO `users` (`name`, `password`, `userid`, `level`) VALUES
+('kavishka', 'Hello@world', 'A000', 1),
+('janaka', 'janaka', 's001', 0),
+('kamal', 'kamal123', 's002', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `registration`
+-- Indexes for table `persons`
 --
-ALTER TABLE `registration`
+ALTER TABLE `persons`
+  ADD PRIMARY KEY (`nid`);
+
+--
+-- Indexes for table `stable`
+--
+ALTER TABLE `stable`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `telephone`
+--
+ALTER TABLE `telephone`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexes for table `trans`
 --
+ALTER TABLE `trans`
+  ADD PRIMARY KEY (`Bill_No`);
 
 --
--- AUTO_INCREMENT for table `registration`
+-- Indexes for table `users`
 --
-ALTER TABLE `registration`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`),
+  ADD UNIQUE KEY `userid` (`userid`);
 COMMIT;
-
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
